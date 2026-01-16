0fd06fc02334efdda90f94120b22df2e12b4c302
Tjungami <utm2477043@stu.o-hara.ac.jp>
2026-01-16 16:08:27 +0900

    Crosを削除

--- config/settings.py
+++ config/settings.py
@@ -28,17 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# CORS
-CORS_ALLOWED_ORIGINS = [
-    "http://100.52.252.209",
-]
-
 # Application definition
 
 INSTALLED_APPS = [
     'daphne',
     'channels',
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +51,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
