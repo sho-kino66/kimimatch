83c1c957df924d64890238554285291881f5ce71
sho-kino66 <utm2477092@stu.o-hara.ac.jp>
2026-02-06 14:17:59 +0900

    settings

--- config/settings.py
+++ config/settings.py
@@ -97,8 +97,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kimimatch_db',
+        'USER': 'kimimatch_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
